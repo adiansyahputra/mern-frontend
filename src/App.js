8eda350d5fe812ade80fa1b2930b0c9f3f8fa9a7
Adiansyah Putra <adiansyahhputra@gmail.com>
2023-02-07 17:26:02 +0700

    alhamdulillah

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Users from './users/pages/Users';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import NewPlace from './places/pages/NewPlace';
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
